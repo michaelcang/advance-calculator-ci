51bb961dab62c3d4ef49f6a0ada1cd83092195a5
michaelcang <michaelcangcianno@gmail.com>
2018-07-24 10:39:13 +0700

    finish app

---
+++ calculator.js
@@ -0,0 +1,50 @@
+'use strict'
+
+class Calculator {
+  //write your code here
+  constructor (num) {
+    this.number = num || 1;
+  }
+  add (num) {
+    this.number += num;
+    return this;
+  }
+  substract (num) {
+    this.number -= num;
+    return this;
+  }
+  multiply (num) {
+    this.number *= num;
+    return this;
+  }
+  divide (num) {
+    this.number /= num;
+    return this;
+  }
+  square () {
+    this.number *= this.number;
+    return this;
+  }
+  toThePowerOf(n) {
+    this.number = Math.pow(this.number, n);
+    return this;
+  }
+  squareRoot () {
+    this.number = Math.sqrt(this.number);
+    return this;
+  }
+
+  nthRootOf(root) {
+    this.number = Math.pow(this.number, 1/root);
+    return this;
+  }
+
+  pi() {
+    this.number *= Math.PI;
+    return this;
+  }
+}
+
+module.exports = {
+  Calculator
+}
